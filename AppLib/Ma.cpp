4af03fd82d9068a1c0aa2c8121dd3038f04beb20
Tonghua Li <tonghualee@hotmail.com>
2018-05-13 22:50:34 -0400

    fixed bugs

--- AppLib/Ma.cpp
+++ AppLib/Ma.cpp
@@ -1,27 +1,25 @@
 #include "Ma.h"
 
 Ma::Ma(QObject *parent, Player player, int x, int y)
-    : ChessPiece(parent, player, "馬")
-{
-    setPosition(x, y);
+    : ChessPiece(parent, player, "馬") {
+  setPosition(x, y);
 }
 
-bool Ma::canMoveTo(const IBoard *board, int x, int y) const
-{
-    auto currentX = _position.x();
-    auto currentY = _position.y();
-    auto deltaY = abs(y - currentY);
-    auto deltaX = abs(x - currentX);
-    if (deltaX * deltaY != 2) {
-        return false;
-    }
-    if (abs(y) > abs(x)) { // Y direction
-        auto p = board->getPlayer(x, y / abs(y) + currentY);
-        return p == Player::None;
-    }
-    if (abs(x) > abs(y)) {
-        auto p = board->getPlayer(x / abs(x) + currentX, y);
-        return p == Player::None;
-    }
+bool Ma::canMoveTo(const IBoard *board, int x, int y) const {
+  auto currentX = _position.x();
+  auto currentY = _position.y();
+  auto deltaY = y - currentY;
+  auto deltaX = x - currentX;
+  if (abs(deltaX * deltaY) != 2) {
     return false;
+  }
+  if (abs(deltaY) > abs(deltaX)) {  // Y direction
+    auto p = board->getPlayer(x, deltaY / abs(deltaY) + currentY);
+    return p == Player::None;
+  }
+  if (abs(deltaY) < abs(deltaX)) {
+    auto p = board->getPlayer(deltaX / abs(deltaX) + currentX, y);
+    return p == Player::None;
+  }
+  return false;
 }
--- Tests/ShiTests.h
+++ Tests/ShiTests.h
@@ -5,7 +5,7 @@
 #include <gtest/gtest.h>
 using namespace testing;
 
-TEST(ShiTest, CannotMoveOutOfBox)
+TEST(ShiTest, canMoveTo)
 {
     auto shi = new Shi(nullptr, Player::Black, 3, 0);
     MockBoard board;
@@ -20,12 +20,5 @@ TEST(ShiTest, CannotMoveOutOfBox)
     ASSERT_FALSE(shi->canMoveTo(&board, 3, 2));
     ASSERT_FALSE(shi->canMoveTo(&board, 3, 1));
     ASSERT_FALSE(shi->canMoveTo(&board, 4, 0));
-    int xPositions[9][2] = {
-        {3, 0}, {4, 0}, {5, 0},
-        {3, 1}, {4, 1}, {5, 1},
-        {3, 2}, {4, 2}, {5, 2},
-    };
-    for(int i =0; i< 9; i++){
-        ASSERT_TRUE(shi->canMoveTo(&board, xPositions[i][0], xPositions[i][1]));
-    }
+
 }
