6323a46711e7bbb0dd71dd438d1fb7190773aa74
Tonghua Li <tonghualee@hotmail.com>
2018-05-25 09:52:26 -0400

    Fixed unit test compiling errors

--- Tests/MockBoard.h
+++ Tests/MockBoard.h
@@ -6,6 +6,9 @@
 class MockBoard: public IBoard{
 public:
     MOCK_CONST_METHOD2(getPlayer, Player(int x, int y));
+    MOCK_CONST_METHOD2(isShuai, bool(int x, int y));
     MOCK_CONST_METHOD2(isCrossRiver, bool(Player player, int y));
+    MOCK_CONST_METHOD3(isInsidePalace, bool(Player player, int x, int y));
+    MOCK_CONST_METHOD2(getPieceId, int(int x, int y));
 };
 #endif // MOCKBOARD_H
